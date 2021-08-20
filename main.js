9164e4936873693cdea63ad367eb521d56c45aa5
Aymen Dirar <63937200+adirar111@users.noreply.github.com>
2021-08-19 20:42:40 -0700

    update august 2021 i want to make a v2 :soon:, but until then, this will do?

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    // adapted from https://codepen.io/lasjorg/pen/GRKreyJ
-    const button = document.querySelector("#play-pause");
-    const audio = document.querySelector("audio");
+  // adapted from https://codepen.io/lasjorg/pen/GRKreyJ
+  const button = document.querySelector("#play-pause");
+  const audio = document.querySelector("audio");
 
-    button.addEventListener("click", () => {
-        console.log("click");
-        if (audio.paused) {
-            audio.volume = 0.4;
-            audio.play();
-        } else {
-            audio.pause();
-        }
-        button.classList.add("fade");
-    });
+  button.addEventListener("click", () => {
+    if (audio.paused) {
+      audio.volume = 0.4;
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    button.classList.add("fade");
+  });
 });
