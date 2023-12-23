0b9760275f7208ba0aee13f98ae004770cafc131
aymen <63937200+adirar111@users.noreply.github.com>
2023-12-23 02:29:02 -0800

    reorg

---
+++ src/photolog-metadata.ts
@@ -0,0 +1,8 @@
+export const PhotoLogMetadata: {[key: string]: {[key: string]: any}} = {
+  "2023": { count: 234 },
+  "2022": { count: 189 },
+  "2021": { count: 41 },
+};
+
+export const generateImageSrcForYear = (year: string): string[] => 
+  Array.from({length: PhotoLogMetadata[year].count}, (_, i) => `/photolog/${year}/${i + 1}.jpeg`).reverse()
