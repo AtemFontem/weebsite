96e7a5780985ed51bfa94689546cfd732e6408e4
aymen <63937200+adirar111@users.noreply.github.com>
2023-12-24 14:11:18 -0800

    any -> number

--- src/photolog-metadata.ts
+++ src/photolog-metadata.ts
@@ -1,4 +1,4 @@
-export const PhotoLogMetadata: {[key: string]: {[key: string]: any}} = {
+export const PhotoLogMetadata: {[key: string]: {[key: string]: number}} = {
   "2023": { count: 234 },
   "2022": { count: 189 },
   "2021": { count: 41 },
