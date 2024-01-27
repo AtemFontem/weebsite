c9c9f51a0929e13beba3b9254a74efd25ece4a96
aymen <63937200+adirar111@users.noreply.github.com>
2024-01-27 12:53:41 -0800

    2024-01-27

--- src/photolog-utils.ts
+++ src/photolog-utils.ts
@@ -1,5 +1,5 @@
 export const PhotoLogMetadata: {[key: string]: {[key: string]: number}} = {
-  "2024": { count: 10 },
+  "2024": { count: 11 },
   "2023": { count: 291 },
   "2022": { count: 246 },
   "2021": { count: 52 },
