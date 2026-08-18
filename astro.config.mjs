511d366d832edd7875607426a0759e80cc5004d0
Atem Fontem <46503410+AtemFontem@users.noreply.github.com>
2026-08-17 21:53:04 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aymen.zip',
+  site: 'https://atemfontem.github.io/weebsite',
 })
