f2f7e2faba1d1911fb50c586e954fc46bf0c45c3
Atem Fontem <atemfontem1@gmail.com>
2026-09-10 15:44:18 -0400

    Skip strict type check in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Make sure output is not set to 'static' only
   // If you have output: 'static', change it to:
-  output: 'server', // or 'hybrid'
+  output: 'static',
 
   site: 'https://atemfontem.github.io/weebsite',
   // ... rest of config
